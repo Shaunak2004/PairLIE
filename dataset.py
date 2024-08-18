976a984610a806f095efbd2fcb7ffd0c77d48239
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-18 08:20:12 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -66,3 +66,22 @@ class DatasetFromFolder(data.Dataset):
     
     def __len__(self):
         return len(self.data_filenames)
+
+class DatasetFromFolderEval(data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        super(DatasetFromFolderEval, self).__init__()
+        data_filenames = [join(data_dir, x) for x in listdir(data_dir) if is_image_file(x)]
+        data_filenames.sort()
+        self.data_filenames = data_filenames
+        self.transform = transform
+
+    def __getitem__(self, index):
+        input = load_img(self.data_filenames[index])
+        _, file = os.path.split(self.data_filenames[index])
+
+        if self.transform:
+            input = self.transform(input)
+        return input, file
+
+    def __len__(self):
+        return len(self.data_filenames)
