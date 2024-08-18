e9e9a32516310424855c1cbe59fabaeecae1d748
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-18 08:32:04 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -34,7 +34,7 @@ class DatasetFromFolder(data.Dataset):
         # List all image files in the directory
         data_filenames = [join(dir_path, x) for x in listdir(dir_path) if is_image_file(x)]
         self.datafilenames = data_filenames
-        num = 946
+        num = 1892
         
         # Check if there are any images in the directory
         if num == 0:
@@ -66,7 +66,7 @@ class DatasetFromFolder(data.Dataset):
 
     
     def __len__(self):
-        return 946
+        return 1892
 
 class DatasetFromFolderEval(data.Dataset):
     def __init__(self, data_dir, transform=None):
